940b0db199c2d3307afcb2c19589a213d22aecba
Walker Dunn-O'Connor <walkerj.dunn@gmail.com>
2017-11-26 23:15:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,4 @@
-var playlist= {musician: "song title"}
+var playlist= {Tswift: "Romeo and Juliet"}
+function updatePlaylist(object,artist name,song title){
+  return Object.assign({}playlist,[artist name],"song title")
+}
